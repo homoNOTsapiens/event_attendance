d9da9af0326770993bbd7ba6e71a46fc514eef6c
homoNOTsapiens <targbot2bella@gmail.com>
2023-05-10 17:32:49 +0300

    translated django

--- manage.py
+++ manage.py
@@ -6,6 +6,4 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
     from django.core.management import execute_from_command_line
-    LANGUAGE_CODE = 'ru-RU'
-    USE_I18N = True
     execute_from_command_line(sys.argv)
