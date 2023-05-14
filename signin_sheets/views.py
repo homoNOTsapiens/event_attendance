1f7db6485ca6911d6a90afa6b5333c1868494eb4
homoNOTsapiens <targbot2bella@gmail.com>
2023-05-15 01:48:36 +0300

    syntax fixes

--- signin_sheets/views.py
+++ signin_sheets/views.py
@@ -41,7 +41,10 @@ class EventCreateView(LoginRequiredMixin, CreateView):
     """Create a new event."""
 
     model = Event
-    fields = ['Название','datetime','description','address','gps_loc','duration']
+    fields = ['name','datetime','description','address','gps_loc','duration']
+    labels = {
+        'name':'НАЗВАНИЕ:'
+    }
     def form_valid(self, form):
         resp = super().form_valid(form)
         self.object.event_admin = get_user(self.request)
