31d8d15029153e1c392e0608cb520ec4d10f3e25
homoNOTsapiens <targbot2bella@gmail.com>
2023-05-22 23:05:19 +0300

    fixes

--- signin_sheets/models.py
+++ signin_sheets/models.py
@@ -37,5 +37,3 @@ class EventParticipant(models.Model):
     group = models.CharField(max_length=255, blank=True,verbose_name='Группа:')
     gps = models.CharField(max_length=255, blank=True)
     event = models.ForeignKey(Event, null=True,verbose_name='Мероприятие:')
-    def __str__(self):
-        return f'{self.first_name} {self.last_name or ""} ({self.email or "[No Email Given]"})'
